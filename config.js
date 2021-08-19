02a690d80d45b0b916ad98099d34ffcf4d3ace94
KidsHat <89117861+KidsHat@users.noreply.github.com>
2021-08-19 15:24:09 +0700

    uplaod

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Kiet ơiiiiiiiii',
+    introDesc: `Tao có cái này muốn hỏi mày nèèèèèè`,
+    btnIntro: 'Nhấn vào đi',
+    title: 'Kiet Co Yeuu Nọc khonggggggg??😙',
+    desc: 'Trả lời thật lòng nhah !! ',
+    btnYes: 'Chọn cái này vẫn là có nhưng màu đỏ nguy hiểm hơn',
+    btnNo: 'Chọn cái này là có',
+    question:'Thật hong dọ😙 ',
+    btnReply: 'Xác Nhận',
+    reply: 'Kiet yeu Nọc lắm mlem mlem mlem mlem mlem mlem mlem mlem mlem mlemmlem mlem mlem mlem mlemmlem mlem mlem mlem mlemmlem mlem mlem mlem mlemmlem mlem mlem mlem mlem',
+    mess: 'Yeu Kịt <3',
+    messDesc: 'yêu Anhhhhhhh❤❤❤❤',
+    btnAccept: 'Nhấn vô điiiiii',
+    messLink: 'https://www.facebook.com/ngoc.phamthibao.921' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
